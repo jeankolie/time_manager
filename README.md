363f28d6c107749543aebad9c83266da1021b980
jeankolie <56737593+jeankolie@users.noreply.github.com>
2019-10-20 11:51:03 +0000

    derniere modiffication pour departement

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Creation du controlleur: php artisan make:controller NomController --resource
-Creation du gestionnaire GestionNom dans app/Gestion
+Creation du gestionnaire: GestionNom dans app/Gestion
 Creation du request pour la creation: php artisan make:requestCreateRequest
 Creation du request pour la modification: php artisan make:requestUpdateRequest
 
-Executer composer dump-autoload si tu as des errerur de classes introuvable
+
+si tu as des errerur de classes introuvable Executer: composer dump-autoload 
+demarrer le serveur: php artisan serve
---
+++ app/Http/Controllers/DepartementController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\{Departement};
+use App\Http\Requests\{DepartementCreateRequest, DepartementUpdateRequest};
+use App\Gestion\{GestionDepartement};
+
+class DepartementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('departement', [
+            'departements' => Departement::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('forms.departement.add');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(DepartementCreateRequest $request, GestionDepartement $gestion)
+    {
+        $gestion->create($request);
+
+        return back()->with('success', "Departement creer avec success");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         return view('forms.departement.update', [
+            'departement' => Departement::whereSlug($id)->first()
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(DepartementUpdateRequest $request, GestionDepartement $gestion, $id)
+    {
+        $gestion->update($request);
+
+        return back()->with('success', "Departement modifier avec success");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
