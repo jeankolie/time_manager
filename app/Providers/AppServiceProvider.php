388482a8cf9beb8f34ee815ee739260b8b369521
jeankolie <56737593+jeankolie@users.noreply.github.com>
2020-01-11 17:17:08 +0000

    test new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,12 +33,12 @@ class AppServiceProvider extends ServiceProvider
         View::share('per', Personnel::count());
 
         config(['app.name' => 'Time manager']);
-        config([
+       /* config([
             'mail.host' =>'time-manager.prestigroupgn.com',
             'mail.port' => 465,
             'mail.username' => 'emplois@time-manager.prestigroupgn.com',
             'mail.password' => 'JeanKolie',
             'mail.encryption' => 'tls'
-        ]);
+        ]);*/
     }
 }
